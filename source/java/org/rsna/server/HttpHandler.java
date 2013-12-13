347b02a687484f2da1479ccd1932c5fe2086bff7
John Perry <johnperry@dls.net>
2013-12-13 06:56:19 -0600

    Add timeout timer to HttpHandler

--- source/java/org/rsna/server/HttpHandler.java
+++ source/java/org/rsna/server/HttpHandler.java
@@ -7,20 +7,27 @@
 
 package org.rsna.server;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.Socket;
+import javax.swing.Timer;
 import org.apache.log4j.Logger;
 import org.rsna.servlets.Servlet;
 
 /**
  * The thread that handles an individual HTTP connection.
  */
-public class HttpHandler extends Thread {
+public class HttpHandler extends Thread implements ActionListener {
 
 	static final Logger logger = Logger.getLogger(HttpHandler.class);
+	static final int timeout = 60000;
 
 	Socket socket;
 	ServletSelector selector;
+	HttpResponse res = null;
+	HttpRequest req = null;
+	Timer timer = null;
 
 	/**
 	 * Construct an HttpHandler.
@@ -37,8 +44,10 @@ public class HttpHandler extends Thread {
 	 * selecting a Servlet to handle the request, and returning the response.
 	 */
 	public void run() {
-		HttpResponse res = null;
-		HttpRequest req = null;
+		//Initialize a timer to log request timeouts
+		timer = new Timer(timeout, this);
+		timer.setRepeats(false);
+
 		try {
 			//Make a response
 			res = new HttpResponse(socket);
@@ -48,6 +57,9 @@ public class HttpHandler extends Thread {
 
 			//Get the Servlet
 			Servlet servlet = selector.getServlet(req);
+
+			//Start the timeout timer
+			timer.start();
 
 			//Call the appropriate method
 			if (req.method.equals("GET")) {
@@ -61,8 +73,12 @@ public class HttpHandler extends Thread {
 				res.send();
 				logger.debug("Unimplemented request ("+req.method+") received from "+req.getRemoteAddress());
 			}
+
+			//Stop the timer; the timeout didn't happen
+			timer.stop();
 		}
 		catch (Exception ex) {
+			timer.stop();
 			if (req != null) {
 				logger.error("Internal server error ("+req.toString()+")",ex);
 				try {
@@ -87,6 +103,11 @@ public class HttpHandler extends Thread {
 		if (res != null) res.close();
 		try { socket.close(); }
 		catch (Exception ex) { logger.debug("Unable to close the socket."); }
+	}
+
+	public void actionPerformed(ActionEvent event) {
+		String reqString = (req != null) ? ": "+req.toString() : "";
+		logger.warn("Request timed out ("+(timeout/1000)+" sec)" + reqString);
 	}
 
 }
